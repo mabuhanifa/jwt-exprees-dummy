899cb8adf97ff3d9192d0697efb9849b84008908
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-02 03:25:02 +0600

    "/login"

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
-const port = process.env.port || 5000;
+const port = 8000;
+
+const posts = [
+  {
+    id: 01,
+    name: "test",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.get("/login", (req, res) => {
+  //Authentication
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
 
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
