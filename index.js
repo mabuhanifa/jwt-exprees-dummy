a93847a77d6b5021c6c5be1d0d59fa17bf04e25e
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-02 04:11:47 +0600

    authenticateToken

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ const posts = [
     id: 01,
     name: "test",
   },
+  {
+    id: 02,
+    name: "Rahim",
+  },
 ];
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, () => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.get("/posts", (req, res) => {
   res.json(posts);
